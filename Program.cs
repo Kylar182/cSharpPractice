9ae09df58929f7c0952dc3266f6b9705df9f0934
Kylar182 <ajbeeman@kfsoftwaresolutions.com>
2018-09-19 07:59:22 -0500

    Initial Practice

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cSharpExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*             List<string> mylist = new List<string>();
+            mylist.Add("Hello");
+            mylist.Add("World");
+
+            Console.WriteLine(mylist[0] + " " + mylist[1]);
+             */
+            HelloWorld myWorld = new HelloWorld();
+            myWorld.SayHello();
+            myWorld.MyNumber = 8;
+            Console.WriteLine(myWorld.MyNumber);
+        }
+        public class HelloWorld
+        {
+            string message = "Hello World";
+/*             public int MyNumber{ get; set; }
+ */
+            private int myNumber;
+
+            public int MyNumber
+            {
+                get{ return myNumber; }
+                set{ myNumber = value; }
+            }
+            public void SayHello()
+            {
+                Console.WriteLine(ChangeMessage());
+            }
+
+            private string ChangeMessage()
+            {
+                return message;
+            }
+        }
+    }
+
+}
